dfd0091c70bac4a077a60c1c7f6db4939fac6a16
Alejandro <tk.jalejandro@gmail.com>
2022-03-23 14:21:51 +0100

    controllers added.

--- todo-app-aspnet/Program.cs
+++ todo-app-aspnet/Program.cs
@@ -1,20 +1,34 @@
+//? Import the things here, dont forget to restart.
+using Microsoft.EntityFrameworkCore;
+// The name of my folder, dont forget.
+using todo_app_aspnet.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// MY TODO CONTEXT
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (builder.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
