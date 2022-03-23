fe529b85c123c51a3256315010267d89a5a514c2
Alejandro <tk.jalejandro@gmail.com>
2022-03-23 13:43:40 +0100

    Add project files.

---
+++ todo-app-aspnet/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace todo_app_aspnet.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Text { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
